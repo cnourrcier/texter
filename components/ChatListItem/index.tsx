529c52b2c1f863e5963d43bab5db9bddeba470d2
Charlie Nourrcier <cnourrcier@hotmail.com>
2021-03-03 21:12:03 -0600

    chats tab

--- components/ChatListItem/index.tsx
+++ components/ChatListItem/index.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { ChatRoom } from "../../types";
+import styles from "./style";
+import moment from "moment";
 
 export type ChatListItemProps = {
     chatRoom: ChatRoom;
 }
 
 const ChatListItem = (props: ChatListItemProps) => {
-    const { chatRoom} = props;
+    const { chatRoom } = props;
+
+    const user = chatRoom.users[1];
+
     return (
-        <View>
-            <Text>{chatRoom.lastMessage.content}</Text>
+        <View style={styles.container}>
+            <View style={styles.lefContainer}>
+                <Image source={{ uri: user.imageUri }} style={styles.avatar}/>
+
+                <View style={styles.midContainer}>
+                    <Text style={styles.username}>{user.name}</Text>
+                    <Text numberOfLines={2} style={styles.lastMessage}>{chatRoom.lastMessage.content}</Text>
+                </View>
+
+            </View>
+
+            <Text style={styles.time}>
+                {moment(chatRoom.lastMessage.createdAt).format("MM/DD/YYYY")}
+            </Text>
         </View>
     )
 };
--- screens/ChatsScreen.tsx
+++ screens/ChatsScreen.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import { View } from '../components/Themed';
 import ChatListItem from "../components/ChatListItem";
+
+import chatRooms from '../data/ChatRooms';
 
 export default function ChatsScreen() {
   return (
     <View style={styles.container}>
-      <ChatListItem chatRoom={{lastMessage: {content: "Changed?"}}}/>
+      <FlatList
+          style={{width: '100%'}}
+          data={chatRooms}
+          renderItem={({ item }) => <ChatListItem chatRoom={item} />}
+          keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
